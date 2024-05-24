2cb7f4f88028393c82c99452b7ce1b37cb0a0e69
roguewww <rogue.c3c@gmail.com>
2024-05-24 20:04:06 +0800

    yihan

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
-    <div class="App">
+    <div class="App" @touchstart="handleTouchStart" @touchmove="handleTouchMove">
         <h1>这里是主APP界面</h1>
     </div>
     <RouterLink to="/page1">page1</RouterLink>
     <RouterLink to="/page2">page2</RouterLink>
     <RouterLink to="/page3">page3</RouterLink>
     
-        <RouterView/>
+    <RouterView/>
     
 </template>
+<script>
+import { useRouter } from 'vue-router'
 
-<script lang="ts" setup>
-    import { RouterView, RouterLink } from 'vue-router';
+export default {
+  setup() {
+    const router = useRouter()
+    let startX = 0
+
+    const handleTouchStart = (e) => {
+      startX = e.touches[0].clientX
+    }
+
+    const handleTouchMove = (e) => {
+      const moveX = e.touches[0].clientX
+      const diffX = moveX - startX
+
+      if (diffX < -50) { // Swipe left
+        router.push('/page1');
+        console.log("page1"); // Replace with your desired route
+      } else if (diffX > 50) { // Swipe right
+        router.push('/page2') // Replace with your desired route
+      }
+    }
+
+    return {
+      handleTouchStart,
+      handleTouchMove
+    }
+  }
+}
 </script>
 
-<style scoped>
-
+<style>
+/* Add your styles here */
 </style>
